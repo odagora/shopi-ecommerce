7de145cd80fc6c1a92a5843dc65a0d3f6f5bff8a
Daniel <oscar.d.gonzalez@gmail.com>
2024-01-04 19:38:33 -0500

    feat: :sparkles: product detail rendering in side menu

--- src/components/Card/index.tsx
+++ src/components/Card/index.tsx
@@ -3,8 +3,9 @@ import { Product } from "@/models/Product";
 import { useAppContext } from "@/hooks/useAppContext";
 import { PlusIcon } from "@heroicons/react/20/solid";
 
-export const Card = (props: Product) => {
-  const { count, setCount, openProductDetail } = useAppContext();
+export const Card = (product: Product) => {
+  const { count, setCount, openProductDetail, setProductToShow } =
+    useAppContext();
 
   function handleClick(
     event: MouseEvent<HTMLButtonElement, globalThis.MouseEvent>
@@ -13,19 +14,24 @@ export const Card = (props: Product) => {
     setCount(count + 1);
   }
 
+  function showProduct(ProductDetail: Product) {
+    openProductDetail();
+    setProductToShow(ProductDetail);
+  }
+
   return (
     <article
       className="bg-white w-56 h-60 rounded-lg"
-      onClick={openProductDetail}
+      onClick={() => showProduct(product)}
     >
       <figure className="relative mb-2 w-full h-4/5">
         <figcaption className="absolute bottom-0 left-0 bg-white/60 rounded-lg text-black text-xs m-2 px-3 py-0.5">
-          {props.category.name}
+          {product.category.name}
         </figcaption>
         <img
           className="w-full h-full object-cover rounded-lg"
-          src={props.category.image}
-          alt={props.title}
+          src={product.category.image}
+          alt={product.title}
           referrerPolicy="no-referrer"
         />
         <button
@@ -36,8 +42,8 @@ export const Card = (props: Product) => {
         </button>
       </figure>
       <p className="flex justify-between">
-        <span className="text-sm font-light">{props.title}</span>
-        <span className="text-lg font-medium">${props.price}</span>
+        <span className="text-sm font-light">{product.title}</span>
+        <span className="text-lg font-medium">${product.price}</span>
       </p>
     </article>
   );
--- src/context/index.tsx
+++ src/context/index.tsx
@@ -1,3 +1,4 @@
+import { Product } from "@/models/Product";
 import {
   Dispatch,
   PropsWithChildren,
@@ -6,28 +7,32 @@ import {
   useState,
 } from "react";
 
+type EmptyObj = Record<PropertyKey, never>;
+
 export interface AppContextProps {
   count: number;
   setCount: Dispatch<SetStateAction<number>>;
   isProductDetailOpen: boolean;
   openProductDetail: () => void;
   closeProductDetail: () => void;
+  productToShow: Product | EmptyObj;
+  setProductToShow: Dispatch<SetStateAction<Product | EmptyObj>>;
 }
 
 export const AppContext = createContext<AppContextProps | undefined>(undefined);
 
 export function AppProvider({ children }: PropsWithChildren) {
+  // Shopping Cart - Increment quantity
   const [count, setCount] = useState<number>(0);
+
+  // Product Detail - Open/Close
   const [isProductDetailOpen, setIsProductDetailOpen] =
     useState<boolean>(false);
+  const openProductDetail = () => setIsProductDetailOpen(true);
+  const closeProductDetail = () => setIsProductDetailOpen(false);
 
-  function openProductDetail() {
-    setIsProductDetailOpen(true);
-  }
-
-  function closeProductDetail() {
-    setIsProductDetailOpen(false);
-  }
+  // Product Detail - Show product
+  const [productToShow, setProductToShow] = useState<Product | EmptyObj>({});
 
   const contextData: AppContextProps = {
     count,
@@ -35,6 +40,8 @@ export function AppProvider({ children }: PropsWithChildren) {
     isProductDetailOpen,
     openProductDetail,
     closeProductDetail,
+    productToShow,
+    setProductToShow,
   };
 
   return (
