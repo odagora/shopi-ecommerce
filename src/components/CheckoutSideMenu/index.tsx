7e3c726ac4e08779b8e131739f83165fb8d84804
Daniel <oscar.d.gonzalez@gmail.com>
2024-01-09 21:26:36 -0500

    feat: :sparkles: OrderCard component and rendering in checkout menu

--- src/components/CheckoutSideMenu/index.tsx
+++ src/components/CheckoutSideMenu/index.tsx
@@ -1,14 +1,16 @@
 import { useAppContext } from "@/hooks/useAppContext";
 import { XMarkIcon } from "@heroicons/react/20/solid";
+import { OrderCard } from "@/components/OrderCard";
 
 export const CheckoutSideMenu = () => {
-  const { isCheckoutSideMenuOpen, closeCheckoutSideMenu } = useAppContext();
+  const { isCheckoutSideMenuOpen, closeCheckoutSideMenu, cartProducts } =
+    useAppContext();
 
   return (
     <aside
       className={`${
         isCheckoutSideMenuOpen ? "flex" : "hidden"
-      } flex-col fixed right-0 border bg-white border-black rounded-lg w-[360px] h-[calc(100vh-80px)] top-[5rem]`}
+      } flex-col fixed right-0 border bg-white border-black rounded-lg w-[360px] h-[calc(100vh-80px)] top-[5rem] overflow-y-scroll`}
     >
       <div className="flex justify-between items-center p-6">
         <h2 className="font-medium text-xl">My Order</h2>
@@ -16,6 +18,16 @@ export const CheckoutSideMenu = () => {
           className="h-6 w-6 cursor-pointer"
           onClick={closeCheckoutSideMenu}
         />
+      </div>
+      <div className="px-6">
+        {cartProducts.map((product) => (
+          <OrderCard
+            key={product.id}
+            imageUrl={product.images[0]}
+            title={product.title}
+            price={product.price}
+          />
+        ))}
       </div>
     </aside>
   );
---
+++ src/components/OrderCard/index.tsx
@@ -0,0 +1,31 @@
+import { XMarkIcon } from "@heroicons/react/20/solid";
+
+interface Props {
+  title: string;
+  price: number;
+  imageUrl: string;
+}
+
+export const OrderCard = (props: Props) => {
+  const { title, price, imageUrl } = props;
+
+  return (
+    <article className="flex justify-between items-center mb-3">
+      <div className="flex items-center gap-2">
+        <figure className="w-20 h-20">
+          <img
+            className="w-full h-full rounded-lg object-cover"
+            src={imageUrl}
+            alt={title}
+            referrerPolicy="no-referrer"
+          />
+        </figure>
+        <p className="text-sm font-light">{title}</p>
+      </div>
+      <div className="flex items-center gap-2">
+        <p className="text-lg font-medium">${price}</p>
+        <XMarkIcon className="h-6 w-6 cursor-pointer" />
+      </div>
+    </article>
+  );
+};
