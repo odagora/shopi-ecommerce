ee91e8d618677b3009135a7a5423824f08c3d556
Daniel <oscar.d.gonzalez@gmail.com>
2024-01-10 21:47:53 -0500

    feat: :sparkles: handle duplicate products in checkout menu

--- src/components/Card/index.tsx
+++ src/components/Card/index.tsx
@@ -1,7 +1,7 @@
 import { MouseEvent } from "react";
 import { Product } from "@/models/Product";
 import { useAppContext } from "@/hooks/useAppContext";
-import { PlusIcon } from "@heroicons/react/20/solid";
+import { CheckIcon, PlusIcon } from "@heroicons/react/20/solid";
 
 export const Card = (product: Product) => {
   const {
@@ -16,30 +16,63 @@ export const Card = (product: Product) => {
     closeCheckoutSideMenu,
   } = useAppContext();
 
-  function handleClick(
-    event: MouseEvent<HTMLButtonElement, globalThis.MouseEvent>
+  function addProductsToCart(
+    event: MouseEvent<HTMLButtonElement, globalThis.MouseEvent>,
+    productData: Product
   ) {
     event.stopPropagation();
     setCount(count + 1);
-    addProductToCart(product);
+    setCartProducts([...cartProducts, productData]);
     openCheckoutSideMenu();
     closeProductDetail();
   }
 
-  function showProduct(ProductDetail: Product) {
-    openProductDetail();
-    closeCheckoutSideMenu();
-    setProductToShow(ProductDetail);
+  function showProduct(
+    event: MouseEvent<HTMLElement, globalThis.MouseEvent>,
+    ProductDetail: Product
+  ) {
+    const target = event.target as HTMLElement;
+    const nodeNames = ["BUTTON", "svg", "path"];
+
+    if (!nodeNames.includes(target.nodeName)) {
+      openProductDetail();
+      closeCheckoutSideMenu();
+      setProductToShow(ProductDetail);
+    }
   }
 
-  function addProductToCart(productData: Product) {
-    setCartProducts([...cartProducts, productData]);
+  function renderIcon(id: number) {
+    const isProductInCart = cartProducts.some((product) => product.id === id);
+    let icon, handleClick, bgColor, textColor;
+
+    if (isProductInCart) {
+      icon = <CheckIcon />;
+      bgColor = "bg-black";
+      textColor = "text-white";
+      handleClick = () => {};
+    } else {
+      icon = <PlusIcon />;
+      bgColor = "bg-white/60";
+      textColor = "text-black";
+      handleClick = (
+        event: MouseEvent<HTMLButtonElement, globalThis.MouseEvent>
+      ) => addProductsToCart(event, product);
+    }
+
+    return (
+      <button
+        className={`absolute top-0 right-0 rounded-full w-6 h-6 m-2 p-1 text-xs ${bgColor} ${textColor}`}
+        onClick={handleClick}
+      >
+        {icon}
+      </button>
+    );
   }
 
   return (
     <article
       className="bg-white w-56 h-60 rounded-lg"
-      onClick={() => showProduct(product)}
+      onClick={(event) => showProduct(event, product)}
     >
       <figure className="relative mb-2 w-full h-4/5">
         <figcaption className="absolute bottom-0 left-0 bg-white/60 rounded-lg text-black text-xs m-2 px-3 py-0.5">
@@ -51,12 +84,7 @@ export const Card = (product: Product) => {
           alt={product.title}
           referrerPolicy="no-referrer"
         />
-        <button
-          className="absolute top-0 right-0 bg-white/60 rounded-full w-6 h-6 m-2 p-1 text-xs"
-          onClick={(event) => handleClick(event)}
-        >
-          <PlusIcon />
-        </button>
+        {renderIcon(product.id)}
       </figure>
       <p className="flex justify-between">
         <span className="text-sm font-light">{product.title}</span>
